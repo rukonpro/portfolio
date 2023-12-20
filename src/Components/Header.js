d4f1a698ea037e126fd9a48c25f00b3bd769e263
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2023-12-21 02:04:37 +0600

    color change

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Iphone from '../Images/header-laptop-2.svg';
+import React from "react";
+import Iphone from "../Images/header-laptop-2.svg";
 import EmailIcon from "../Images/gmail.png";
 import PhoneIcon from "../Images/phone-call.png";
 import SkypeIcon from "../Images/skype.png";
@@ -8,126 +8,196 @@ import Whatsapp from "../Images/whatsapp.png";
 import LazyImageLoader from "../utilitis/LazyImageLoader";
 
 const Header = () => {
+  return (
+    <header
+      id="home"
+      className="relative pb-10 bg-[#0e0e36]     overflow-hidden"
+    >
+      <div className="relative z-10 md:px-28">
+        <div className="lg:flex flex-none justify-between container mx-auto md:pt-28 pt-28 md:px-0 px-5  ">
+          <div>
+            <div>
+              <div>
+                <span className="text-2xl font-bold bg-gradient-to-r from-[#816aff] to-[#d066fd] text-transparent bg-clip-text tracking-[5px]">
+                  Hello!, I'm
+                </span>
+                <h1 className=" md:text-7xl text-4xl font-bold font-['Roboto'] pt-2  ">
+                  <span className="bg-gradient-to-r from-[#9e31f7ff] to-[#344dedff] text-transparent bg-clip-text tracking-[4px] ">
+                    Rukon Uddin
+                  </span>
+                </h1>
 
-    return (
-        <header id="home" className='relative pb-10 bg-[#0e0e36]     overflow-hidden'>
+                <p className=" md:text-xl text-base font-bold font-['Roboto'] mt-2 md:tracking-[5px] tracking-[2px] text-[#fccdff]">
+                  MERN-stack Developer | React Developer | Javascript Developer
+                </p>
 
-            <div className="relative z-10 md:px-28">
-                <div className='lg:flex flex-none justify-between container mx-auto md:pt-28 pt-28 md:px-0 px-5  '>
-                    <div>
-                        <div>
-                            <div>
-                                <span
-                                    className="text-2xl font-bold bg-gradient-to-r from-[#816aff] to-[#d066fd] text-transparent bg-clip-text tracking-[5px]">
-                                    Hello!, I'm
-                                </span>
-                                <h1 className=" md:text-7xl text-4xl font-bold font-['Roboto'] pt-2  "><
-                                    span className="bg-gradient-to-br from-[#816aff] to-[#d066fd] text-transparent bg-clip-text tracking-[4px] ">Rukon Uddin</span>
-                                </h1>
-
-                                <p className=" md:text-xl text-base font-bold font-['Roboto'] mt-2 md:tracking-[5px] tracking-[2px] text-[#fccdff]">MERN-stack
-                                    Developer | React Developer |  Javascript Developer</p>
-
-                                <div className='flex gap-4 pt-5'>
-                                    <a href="https://www.linkedin.com/in/rukonpro/">
-                                        <div
-                                            className=' w-10 h-10 rounded-full flex justify-center items-center bg-[#fbcdff]'>
-                                            <LazyImageLoader>
-                                            <img src={LinkdinIcon || undefined} height="100%" width="100%" alt="linkdin" loading="lazy"/>
-                                            </LazyImageLoader>
-                                        </div>
-                                    </a>
-
-                                    <a href="mailto:rukon.js@gamil.com">
-                                        <div
-                                            className=' w-10 h-10 rounded-full flex justify-center items-center bg-[#fbcdff]'>
-                                            <LazyImageLoader>
-                                                <img src={EmailIcon || undefined} height="100%" width="100%" alt="email" loading="lazy"/>
-                                            </LazyImageLoader>
-                                        </div>
-                                    </a>
-
-                                    <a href="https://join.skype.com/invite/yXu1Kxuk8hZF?fbclid=IwAR18ICJ_AmaQUWEIsiIfd4XyikyYvhcEIrM5HLImwXXJc42KT6pwqWjDaC0">
-                                        <div
-                                            className=' w-10 h-10 rounded-full flex justify-center items-center bg-[#fbcdff]'>
-                                            <LazyImageLoader>
-                                            <img src={SkypeIcon || undefined} height="100%" width="100%" alt="skype" loading="lazy"/>
-                                            </LazyImageLoader>
-                                        </div>
-                                    </a>
-                                    <a href="https://api.whatsapp.com/send?phone=1765459224">
-                                        <div
-                                            className=' w-10 h-10 rounded-full flex justify-center items-center bg-[#fbcdff]'>
-                                            <LazyImageLoader>
-                                                <img src={Whatsapp || undefined} height="100%" width="100%" alt="whatsapp" loading="lazy"/>
-                                            </LazyImageLoader>
-
-                                        </div>
-                                    </a>
-
-                                    <a href="tel:+8801765459224">
-                                        <div
-                                            className=' w-10 h-10 rounded-full flex justify-center items-center bg-[#fbcdff]'>
-                                            <LazyImageLoader>
-                                               <img src={PhoneIcon || undefined} height="100%" width="100%" alt="phone number" loading="lazy"/>
-                                            </LazyImageLoader>
-                                        </div>
-                                    </a>
-                                </div>
-                                <a href="https://drive.google.com/file/d/1S8dFRWC7f3QH6ZVnGp6miYLwPz8s4coh/view?usp=sharing">
-                                    <button
-                                        id="downloadcv"
-                                        aria-label="Download CV"
-                                        className=" mt-5  text-center text-white md:text-xl text-base font-bold font-['Roboto'] bg-gradient-to-l from-[#1355b7] to-[#0e0e36] rounded-full active:bg-gradient-to-l hover:shadow-2xl  hover:shadow-[#263458] tracking-[1px] px-4 py-2 ">
-                                        <span
-                                            className="bg-gradient-to-r from-[#816aff] to-[#d066fd] text-transparent bg-clip-text"> Download CV</span>
-
-                                    </button>
-                                </a>
-                            </div>
-                        </div>
+                <div className="flex gap-4 pt-5">
+                  <a href="https://www.linkedin.com/in/rukonpro/">
+                    <div className=" w-10 h-10 rounded-full flex justify-center items-center bg-[#fbcdff]">
+                      <LazyImageLoader>
+                        <img
+                          src={LinkdinIcon || undefined}
+                          height="100%"
+                          width="100%"
+                          alt="linkdin"
+                          loading="lazy"
+                        />
+                      </LazyImageLoader>
                     </div>
+                  </a>
 
-                    <div className='flex justify-end'>
-                        <LazyImageLoader>
-                            <img className=' md:w-[76vh]' src={Iphone || undefined} height="100%" width="100%" alt='bannderphoto' fetchpriority="high" loading="eager"/>
-                        </LazyImageLoader>
+                  <a href="mailto:rukon.js@gamil.com">
+                    <div className=" w-10 h-10 rounded-full flex justify-center items-center bg-[#fbcdff]">
+                      <LazyImageLoader>
+                        <img
+                          src={EmailIcon || undefined}
+                          height="100%"
+                          width="100%"
+                          alt="email"
+                          loading="lazy"
+                        />
+                      </LazyImageLoader>
                     </div>
-                </div>
+                  </a>
 
-                <div className='flex justify-center'>
-                    <button
-                        onClick={()=>window.scrollBy(0, 600)}
+                  <a href="https://join.skype.com/invite/yXu1Kxuk8hZF?fbclid=IwAR18ICJ_AmaQUWEIsiIfd4XyikyYvhcEIrM5HLImwXXJc42KT6pwqWjDaC0">
+                    <div className=" w-10 h-10 rounded-full flex justify-center items-center bg-[#fbcdff]">
+                      <LazyImageLoader>
+                        <img
+                          src={SkypeIcon || undefined}
+                          height="100%"
+                          width="100%"
+                          alt="skype"
+                          loading="lazy"
+                        />
+                      </LazyImageLoader>
+                    </div>
+                  </a>
+                  <a href="https://api.whatsapp.com/send?phone=1765459224">
+                    <div className=" w-10 h-10 rounded-full flex justify-center items-center bg-[#fbcdff]">
+                      <LazyImageLoader>
+                        <img
+                          src={Whatsapp || undefined}
+                          height="100%"
+                          width="100%"
+                          alt="whatsapp"
+                          loading="lazy"
+                        />
+                      </LazyImageLoader>
+                    </div>
+                  </a>
 
-                        type="button"
-                        id="mouseButton"
-                        aria-label="mouseButton"
-                        className="mouseButton ">
-                        <svg width="45" height="77" viewBox="0 0 45 77" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="22.1461" cy="14.6504" r="6.47345" fill="url(#paint0_linear_128_381)"/>
-                            <rect x="0.5" y="0.5" width="43.9734" height="76" rx="21.9867"
-                                  stroke="url(#paint1_linear_128_381)"/>
-                            <defs>
-                                <linearGradient id="paint0_linear_128_381" x1="9.70489" y1="10.908" x2="29.4287"
-                                                y2="17.5837" gradientUnits="userSpaceOnUse">
-                                    <stop stopColor="#FC4A1A"/>
-                                    <stop offset="1" stopColor="#F7B733"/>
-                                </linearGradient>
-                                <linearGradient id="paint1_linear_128_381" x1="22.4867" y1="0" x2="22.4867" y2="77"
-                                                gradientUnits="userSpaceOnUse">
-                                    <stop stopColor="#FC4A1A"/>
-                                    <stop offset="1" stopColor="#F7B733"/>
-                                </linearGradient>
-                            </defs>
-                        </svg>
-                    </button>
+                  <a href="tel:+8801765459224">
+                    <div className=" w-10 h-10 rounded-full flex justify-center items-center bg-[#fbcdff]">
+                      <LazyImageLoader>
+                        <img
+                          src={PhoneIcon || undefined}
+                          height="100%"
+                          width="100%"
+                          alt="phone number"
+                          loading="lazy"
+                        />
+                      </LazyImageLoader>
+                    </div>
+                  </a>
                 </div>
+                <a href="https://drive.google.com/file/d/1S8dFRWC7f3QH6ZVnGp6miYLwPz8s4coh/view?usp=sharing">
+                  <button
+                    id="downloadcv"
+                    aria-label="Download CV"
+                    className=" mt-5  text-center text-white md:text-xl text-base font-bold font-['Roboto'] bg-gradient-to-l from-[#1355b7] to-[#0e0e36] rounded-full active:bg-gradient-to-l hover:shadow-2xl  hover:shadow-[#263458] tracking-[1px] px-4 py-2 "
+                  >
+                    <span className="bg-gradient-to-r from-[#816aff] to-[#d066fd] text-transparent bg-clip-text">
+                      {" "}
+                      Download CV
+                    </span>
+                  </button>
+                </a>
+              </div>
             </div>
-            <div
-                className="absolute custom-animate-pulse inset-0 m-auto max-w-xs h-[357px] blur-[118px] sm:max-w-md md:max-w-lg"
-                style={{background: "linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)"}}></div>
-        </header>
-    );
+          </div>
+
+          <div className="flex justify-end">
+            <LazyImageLoader>
+              <img
+                className=" md:w-[76vh]"
+                src={Iphone || undefined}
+                height="100%"
+                width="100%"
+                alt="bannderphoto"
+                fetchpriority="high"
+                loading="eager"
+              />
+            </LazyImageLoader>
+          </div>
+        </div>
+
+        <div className="flex justify-center">
+          <button
+            onClick={() => window.scrollBy(0, 600)}
+            type="button"
+            id="mouseButton"
+            aria-label="mouseButton"
+            className="mouseButton "
+          >
+            <svg
+              width="45"
+              height="77"
+              viewBox="0 0 45 77"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="22.1461"
+                cy="14.6504"
+                r="6.47345"
+                fill="url(#paint0_linear_128_381)"
+              />
+              <rect
+                x="0.5"
+                y="0.5"
+                width="43.9734"
+                height="76"
+                rx="21.9867"
+                stroke="url(#paint1_linear_128_381)"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_128_381"
+                  x1="9.70489"
+                  y1="10.908"
+                  x2="29.4287"
+                  y2="17.5837"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#FC4A1A" />
+                  <stop offset="1" stopColor="#F7B733" />
+                </linearGradient>
+                <linearGradient
+                  id="paint1_linear_128_381"
+                  x1="22.4867"
+                  y1="0"
+                  x2="22.4867"
+                  y2="77"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#FC4A1A" />
+                  <stop offset="1" stopColor="#F7B733" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div
+        className="absolute custom-animate-pulse inset-0 m-auto max-w-xs h-[357px] blur-[118px] sm:max-w-md md:max-w-lg"
+        style={{
+          background:
+            "linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)",
+        }}
+      ></div>
+    </header>
+  );
 };
 
 export default Header;
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,120 +1,146 @@
-import {useState} from "react";
-import {Link, NavLink} from 'react-router-dom';
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import MenuBar from "../Images/menu-bar.png";
 import Close from "../Images/close.png";
 
 const Navigation = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
+  const navLinks = [
+    {
+      title: "Home",
+      path: "/#home",
+    },
+    {
+      title: "About",
+      path: "/#about",
+    },
+    {
+      title: "Skills",
+      path: "/#skills",
+    },
+    {
+      title: "Services",
+      path: "/#services",
+    },
 
-    const navLinks = [
-        {
-            title: "Home",
-            path: "/#home"
-        },
-        {
-            title: "About",
-            path: "/#about"
-        },
-        {
-            title: "Skills",
-            path: "/#skills"
-        },
-        {
-            title: "Services",
-            path: "/#services"
-        },
+    {
+      title: "Portfolio",
+      path: "/#portfolio",
+    },
+    {
+      title: "Blogs",
+      path: "/#blogs",
+    },
+    {
+      title: "Contact",
+      path: "/#contact",
+    },
+  ];
+  const menuHandler = () => setMenuOpen((value) => !value);
 
-        {
-            title: "Portfolio",
-            path: "/#portfolio"
-        },
-        {
-            title: "Blogs",
-            path: "/#blogs"
-        },
-        {
-            title: "Contact",
-            path: "/#contact"
-        },
-    ]
-    const menuHandler = () => setMenuOpen(value => !value);
-
-
-    return (
-        <nav className="fixed z-50   w-screen">
-            <div
-                className={`  ${menuOpen ? " m-3 rounded-xl bg-gradient-to-tr from-[#030b55ec] to-[#994cd0f5]" +
-                    " lg:from-[#0a0e36] lg:to-[#0a0e36] lg:m-0 lg:rounded-none" : " bg-gradient-to-tr from-[#0a0e36] to-[#0a0e36] "} ${!menuOpen && " bg-[#0a0e36]"}`}>
-                <div>
-                    <div className="flex  justify-between items-center md:px-8 px-5 py-4   ">
-                        <div >
-                            <Link to="/#home" >
-                                <h1 className="bg-gradient-to-br from-[#816aff] to-[#d066fd] text-transparent bg-clip-text text-[22px] font-bold font-['Roboto'] tracking-[2px] ">
-                                    Rukon.PRO
-                                </h1>
-
-                            </Link>
-
-                        </div>
-
-                        <div className=" hidden lg:block">
-
-                            <div>
-                                <ul className="flex items-center gap-5 text-white font-bold px-5 text-xl  ">
-                                    {
-                                        navLinks?.map((nav, index) =>
-                                            <li className="animated-button"  key={index}>
-                                                <a href={nav?.path}>
-                                                    <button className="tracking-[2px] text-[#fccdff]">
-                                                        {nav?.title}
-                                                    </button>
-                                                </a>
-                                            </li>
-                                        )
-                                    }
-                                    <li>
-                                        <button className=" bg-gradient-to-r from-[#0a0cd00d] to-[#9a4cd09d] px-5 py-1 rounded-lg  animated-button tracking-[3px]  text-[#fccdff]" onClick={() => document.getElementById("loginModal").showModal()}> <span className="bg-gradient-to-r from-[#816aff] to-[#d066fd] text-transparent bg-clip-text">Login</span>
-                                        </button>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <button id="threeDotButton" onClick={menuHandler}
-                                className="lg:hidden p-2  bg-gradient-to-tr from-[#030b55ec] to-[#994cd0f5] rounded-full block">
-                            {
-                                !menuOpen ? <img className="h-5 w-5" src={MenuBar || undefined} alt="icon"/> :
-                                    <img className="h-5 w-5" src={Close || undefined} alt="icon"/>
-                            }
-                        </button>
-                    </div>
-                </div>
-
-                <div className={`lg:hidden p-1 rounded-b-xl  ${menuOpen ? "block" : "hidden"}`}>
-
-
-                    <ul className=" text-white font-bold  text-xl   ">
-                        {
-                            navLinks?.map((nav, index) =>
-                                <li key={index} >
-                                    <NavLink to={nav?.path}
-                                             onClick={() => setMenuOpen(false)}
-                                             className="animated-button w-full rounded-full inline-block transition-opacity hover:bg-gradient-to-l hover:transition-opacity   from-[#9a4cd00d] to-[#9a4cd03d] px-5 py-3 tracking-[2px] text-[#fccdff]">{nav?.title}</NavLink>
-                                </li>
-                            )
-                        }
-                        <li className=" bg-gradient-to-l from-[#0a0cd00d] to-[#9a4cd09d] px-5 py-3 rounded-full my-3 animated-button w-full">
-                            <button type="button" id="loginbuttion1"
-                                    onClick={() => document.getElementById("loginModal").showModal()}
-                                    className="w-full tracking-[3px] "><span
-                                className="bg-gradient-to-r from-[#816aff] to-[#d066fd] text-transparent bg-clip-text">Login</span>
-                            </button>
-                        </li>
-                    </ul>
-                </div>
+  return (
+    <nav className="fixed z-50   w-screen">
+      <div
+        className={`  ${
+          menuOpen
+            ? " m-3 rounded-xl bg-gradient-to-tr from-[#030b55ec] to-[#994cd0f5]" +
+              " lg:from-[#0a0e36] lg:to-[#0a0e36] lg:m-0 lg:rounded-none"
+            : " bg-gradient-to-tr from-[#0a0e36] to-[#0a0e36] "
+        } ${!menuOpen && " bg-[#0a0e36]"}`}
+      >
+        <div>
+          <div className="flex  justify-between items-center md:px-8 px-5 py-4   ">
+            <div>
+              <Link to="/#home">
+                <h1 className="bg-gradient-to-r from-[#6f0afcff] to-[#344dedff] text-transparent bg-clip-text text-[25px] font-bold font-['Roboto'] tracking-[2px] ">
+                  Rukon.Pro
+                </h1>
+              </Link>
             </div>
-        </nav>
-    );
+
+            <div className=" hidden lg:block">
+              <div>
+                <ul className="flex items-center gap-5 text-white font-bold px-5 text-xl  ">
+                  {navLinks?.map((nav, index) => (
+                    <li className="animated-button" key={index}>
+                      <a href={nav?.path}>
+                        <button className="tracking-[2px] text-[#fccdff]">
+                          {nav?.title}
+                        </button>
+                      </a>
+                    </li>
+                  ))}
+                  <li>
+                    <button
+                      className=" bg-gradient-to-r from-[#0a0cd00d] to-[#9a4cd09d] px-5 py-1 rounded-lg  animated-button tracking-[3px]  text-[#fccdff]"
+                      onClick={() =>
+                        document.getElementById("loginModal").showModal()
+                      }
+                    >
+                      {" "}
+                      <span className="bg-gradient-to-r from-[#6f0afcff] to-[#344dedff] text-transparent bg-clip-text">
+                        Login
+                      </span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <button
+              id="threeDotButton"
+              onClick={menuHandler}
+              className="lg:hidden p-2  bg-gradient-to-tr from-[#5b3491ff] to-[#994cd0f5] rounded-full block"
+            >
+              {!menuOpen ? (
+                <img
+                  className="h-5 w-5"
+                  src={MenuBar || undefined}
+                  alt="icon"
+                />
+              ) : (
+                <img className="h-5 w-5" src={Close || undefined} alt="icon" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        <div
+          className={`lg:hidden p-1 rounded-b-xl  ${
+            menuOpen ? "block" : "hidden"
+          }`}
+        >
+          <ul className=" text-white font-bold  text-xl   ">
+            {navLinks?.map((nav, index) => (
+              <li key={index}>
+                <NavLink
+                  to={nav?.path}
+                  onClick={() => setMenuOpen(false)}
+                  className="animated-button w-full rounded-full inline-block transition-opacity hover:bg-gradient-to-l hover:transition-opacity   from-[#9a4cd00d] to-[#9a4cd03d] px-5 py-3 tracking-[2px] text-[#fccdff]"
+                >
+                  {nav?.title}
+                </NavLink>
+              </li>
+            ))}
+            <li className=" bg-gradient-to-l from-[#0a0cd00d] to-[#9a4cd09d] px-5 py-3 rounded-full my-3 animated-button w-full">
+              <button
+                type="button"
+                id="loginbuttion1"
+                onClick={() =>
+                  document.getElementById("loginModal").showModal()
+                }
+                className="w-full tracking-[3px] "
+              >
+                <span className="bg-gradient-to-r from-[#816aff] to-[#d066fd] text-transparent bg-clip-text">
+                  Login
+                </span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
