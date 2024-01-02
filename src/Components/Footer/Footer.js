6c894086591d898ca29a53962b0b102ba8cdae87
RnRukon <rukon.js@gmail.com>
2024-01-02 22:15:01 +0600

    update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="relative bg-gray-900">
+        <footer className="relative overflow-hidden  flex justify-center items-center bg-[#0e0e36] ">
             <div className="absolute custom-animate-pulse inset-0 blur-[118px] radial-gradient"></div>
-            <div className="absolute flex justify-center py-5 text-white z-10 relative">
+            <div className=" flex justify-center py-5 text-white z-10 ">
                 <h1 className="text-gray-400">Copy right &copy;{new Date().getFullYear()} <span
                     className="bg-gradient-to-br from-[#816aff] to-[#d066fd] text-transparent bg-clip-text ">Rukon.Pro</span>
                 </h1>
